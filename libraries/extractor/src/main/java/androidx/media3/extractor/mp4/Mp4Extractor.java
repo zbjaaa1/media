ca33a31fdbd796c42efc8f962ba3c7d6bfdfb531
zbjaaa1 <168157190+zbjaaa1@users.noreply.github.com>
2026-02-02 17:56:33 +0800

    Update Mp4Extractor.java

--- libraries/extractor/src/main/java/androidx/media3/extractor/mp4/Mp4Extractor.java
+++ libraries/extractor/src/main/java/androidx/media3/extractor/mp4/Mp4Extractor.java
@@ -924,8 +924,8 @@ public final class Mp4Extractor implements Extractor {
           nalPrefix.setPosition(0);
           int nalLengthInt = nalPrefix.readInt();
           if (nalLengthInt < 0) {
-            throw ParserException.createForMalformedContainer(
-                "Invalid NAL length", /* cause= */ null);
+            sampleCurrentNalBytesRemaining = 0;
+  return RESULT_CONTINUE;
           }
           sampleCurrentNalBytesRemaining =
               nalLengthInt - numberOfBytesToDetermineSampleDependencies;
